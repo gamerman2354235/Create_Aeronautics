dd93bc92cd1c31468a4458b5e8c7029c32050adb
FortressNebula <prasun.nandan0909@gmail.com>
2022-01-31 18:01:19 +0000

    cleaned up main mod class

--- src/main/java/com/eriksonn/createaeronautics/CreateAeronautics.java
+++ src/main/java/com/eriksonn/createaeronautics/CreateAeronautics.java
@@ -64,47 +64,16 @@ public class CreateAeronautics
     }
 
     private void setup(final FMLCommonSetupEvent event)
-    {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-    }
+    {}
 
-    private void doClientStuff(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
-    }
+    private void doClientStuff(final FMLClientSetupEvent event) {}
 
     private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("createaeronautics", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
-    }
+    {}
 
-    private void processIMC(final InterModProcessEvent event)
-    {
-        // some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m->m.getMessageSupplier().get()).
-                collect(Collectors.toList()));
-    }
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
+    private void processIMC(final InterModProcessEvent event) {}
 
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
-    }
+    @SuppressWarnings("deprecation")
     public static CreateRegistrate registrate() {
 
         LOGGER.info("Registrate created");
